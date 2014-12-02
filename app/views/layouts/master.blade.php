e2f57aab51dd7ce85af0f610174831611b70a840
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-02 08:34:54 -0600

    Added master template.

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>apply.codeup.com</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <style type="text/css">
+
+    body {
+        padding-top: 20px;
+    }
+
+    </style>
+
+    @yield('header')
+
+</head>
+<body>
+
+    @yield('content')
+
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    @yield('footer')
+
+</body>
+</html>
