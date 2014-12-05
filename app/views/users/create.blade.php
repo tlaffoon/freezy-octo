59bb01e26595f3739a3bc2340c7e92fb1f5fce36
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-04 23:33:26 -0600

    Implemented rudimentary js validation for registration button enable.

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -7,17 +7,10 @@
 
             <img class="img-responsive img-rounded" src="/includes/img/codeup-logo.jpg">
 
-            <h2 class="page-header text-center">New User Registration</h2>
+            <h2 class="page-header">Register An Account</h2>
 
             {{ Form::open(array('action' => array('UsersController@store'), 'class'=>'form', 'role'=>'form', 'method' => 'POST' )) }}
                 
-                <!-- Refactor to use individual error messages. -->
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-
                 <!-- First Name -->
                 {{ Form::label('first', 'First Name') }}
                 {{ Form::text('first', Input::old('first'), array('class' => 'form-group form-control', 'placeholder' => 'First Name')) }}
@@ -28,16 +21,16 @@
                 {{ Form::text('last', Input::old('last'), array('class' => 'form-group form-control', 'placeholder' => 'Last Name')) }}
                 {{ $errors->first('last', '<span class="help-block"><p class="text-warning">:message</p></span><br>') }}
 
-                <!-- Phone -->
-                {{ Form::label('phone', 'Phone') }}
-                {{ Form::text('phone', Input::old('phone'), array('class' => 'form-group form-control', 'placeholder' => '#')) }}
-                {{ $errors->first('phone', '<span class="help-block"><p class="text-warning">:message</p></span><br>') }}
-
                 <!-- Email -->
                 {{ Form::label('email', 'Email') }}
                 {{ Form::text('email', Input::old('email'), array('class' => 'form-group form-control', 'placeholder' => 'Email')) }}
                 {{ $errors->first('email', '<span class="help-block"><p class="text-warning">:message</p></span><br>') }}
                 
+                <!-- Phone -->
+                {{ Form::label('phone', 'Phone') }}
+                {{ Form::text('phone', Input::old('phone'), array('class' => 'form-group form-control', 'placeholder' => '#')) }}
+                {{ $errors->first('phone', '<span class="help-block"><p class="text-warning">:message</p></span><br>') }}
+ 
                 <!-- Password -->
                 {{ Form::label('password', 'Password') }}
                 {{ Form::password('password', array('class' => 'form-group form-control', 'placeholder' => 'Password')) }}
@@ -48,8 +41,9 @@
                 {{ Form::password('password_confirm', array('class' => 'form-group form-control', 'placeholder' => 'Confirm Password')) }}
                 {{ $errors->first('password_confirm', '<span class="help-block"><p class="text-warning">:message</p></span><br>') }}
 
-                {{ Form::submit('Register', array('class' => 'btn btn-default btn-success btn-block')) }}
-                {{ Form::close() }}
+                {{ Form::submit('Register', array('id' => 'registerBtn', 'class' => 'btn btn-default btn-success btn-block disabled')) }}
+            
+            {{ Form::close() }}
 
         </div> <!-- End Column -->
     </div> <!-- End Row -->
@@ -57,37 +51,61 @@
 
 @stop
 
-@section('bottomscript')
+@section('footer')
 <script type="text/javascript">
     
-    // Validate password fields are same with javascript
+    $(document).ready(function() {
+        
+        var first = '';
+        var last = '';
+        var phone = '';
+        var email = '';
+        var password = '';
+        var password_confirm = '';
 
-    function checkPassword(str)
-    {
-      var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-      return re.test(str);
-    }
+        function btnCheck(){
+            if (first && last && phone && email && password.length >= 8) {
+                if (password === password_confirm) {
+                    $('#registerBtn').removeClass('disabled');
+                };
 
-    function checkForm(form)
-    {
-
-        // check password value 1 against password value 2
-        // return true if same
-        // enable registration button
-
-      if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
-        if(!checkPassword(form.pwd1.value)) {
-          alert("The password you have entered is not valid!");
-          form.pwd1.focus();
-          return false;
+            } 
+            else if (password.length < 8 || password !== password_confirm) {
+                $('#registerBtn').addClass('disabled');  
+            }
         }
-      } else {
-        alert("Error: Please check that you've entered and confirmed your password!");
-        form.pwd1.focus();
-        return false;
-      }
-      return true;
-    }
+
+        $('#first').on('keyup', function() {
+            first = $(this).val();
+            btnCheck();
+        });
+
+        $('#last').on('keyup', function() {
+            last = $(this).val();
+            btnCheck();
+        });
+
+        $('#email').on('keyup', function() {
+            email = $(this).val();
+            btnCheck();
+        });
+
+        $('#phone').on('keyup', function() {
+            phone = $(this).val();
+            btnCheck();
+        });
+
+        $('#password').on('keyup', function() {
+            password = $(this).val();
+            btnCheck();
+        });
+
+        $('#password_confirm').on('keyup', function() {
+            password_confirm = $(this).val();
+            btnCheck();
+        });
+    });
+
 </script>
 
 
