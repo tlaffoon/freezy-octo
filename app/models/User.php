8c5b925261610c0989c44730cf5fce004dfe5b97
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-05 23:06:05 -0600

    Added format password function and re-formatted comments throughout.  Improved validation requirements.

--- app/models/User.php
+++ app/models/User.php
@@ -21,23 +21,37 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 *
 	 * @var array
 	 */
-	protected $hidden = array('first', 'last', 'email', 'phone','password');
+	protected $hidden = array('first', 'last', 'email', 'phone','password', 'password_confirm');
 
+
+	/**
+	 * The required fields on users.create form submission.
+	 *
+	 * @var array
+	 */
 	public static $rules = array(
-	    'first'		=> 'required',
-	    'last'		=> 'required',
-	    'phone'		=> 'required',
-	    'email'		=> 'required|email',
-	    'password'	=> 'required'
+
+		'firstname'			=>	'required|alpha|min:2',
+		'lastname'			=>	'required|alpha|min:2',
+		'email'				=>	'required|email|unique:users',
+		'password'			=>	'required|alpha_num|between:6,12|confirmed',
+		'password_confirm'	=>	'required|alpha_num|between:6,12'
 	);
 
 
+	/**
+	 * The directory for image uploads.
+	 *
+	 * @var string
+	 */
 	protected $imgDir = 'img-upload';
 
-	public function setPasswordAttribute($value) {
-		$this->attributes['password'] = Hash::make($value);
-	}
 
+	/**
+	 * The function to handle image uploads.
+	 *
+	 * @param  string  $image
+	 */
 	public function addUploadedImage($image) {
 	    $systemPath = public_path() . '/' . $this->imgDir . '/';
 	    //$imageName = $this->id . '_' . $image->getClientOriginalName();
@@ -45,5 +59,27 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	    $image->move($systemPath, $imageName);
 	    $this->img_path = '/' . $this->imgDir . '/' . $imageName;
 	}
+
+
+	/**
+	 * The function to format password on user creation.
+	 *
+	 * @param  string  $value
+	 */
+	public function setPasswordAttribute($value) {
+		$this->attributes['password'] = Hash::make($value);
+	}
+
+
+	/**
+	 * The function to format phone number on user creation.
+	 *
+	 * @param  string  $value
+	 */
+	public function setPhoneAttribute($value)
+	{
+		$this->attributes['phone'] = '(' . substr($value, 0 , 3 ) . ')' . '-' . substr($value, 3 , 3 ) . '-' . substr($value, 6 , 4 );
+	}
+
 
 }
