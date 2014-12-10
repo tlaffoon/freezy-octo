e1e33004fe1a7d7bb6589e08397ccc1fff3fff0a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-10 11:41:23 -0600

    Added mail config, but needs password for info@codeup.com.

--- app/config/mail.php
+++ app/config/mail.php
@@ -28,7 +28,7 @@ return array(
 	|
 	*/
 
-	'host' => 'smtp.mailgun.org',
+	'host' => 'smtp.gmail.org',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'info@codeup.com', 'name' => 'Codeup'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => null,
+	'username' => 'info@codeup.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => null,
+	'password' => 'codeup2014',
 
 	/*
 	|--------------------------------------------------------------------------
