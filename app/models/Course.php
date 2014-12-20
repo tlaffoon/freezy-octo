57a7d667775793b0da40b8a9223aee87078fced6
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-20 13:44:15 -0600

    Altered Course model.

---
+++ app/models/Course.php
@@ -0,0 +1,22 @@
+<?php
+
+class Course extends Eloquent {
+
+    protected $table = 'courses';
+
+    protected $hidden = ['name', 'description', 'duration', 'current_students', 'max_students', 'cost', 'start_date', 'end_date', 'demo_date'];
+
+    public static $rules = [
+        'name' => 'required|alpha',
+        'description' => 'required|alphanum',
+        'cost' => 'required|integer',
+        'max_students' => 'required|integer',
+        'start_date' => 'required|date',
+        'end_date' => 'required|date'
+    ];
+
+    public function applications() {
+        return $this->hasMany('Application');
+    }
+
+}
