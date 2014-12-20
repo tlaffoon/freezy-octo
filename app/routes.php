7a8b09d84616012d94a0bb1b56b244a189319975
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-20 13:48:09 -0600

    Altered routes.

--- app/routes.php
+++ app/routes.php
@@ -19,6 +19,14 @@
 //  Users Resource Route
 Route::resource('users', 'UsersController');
 
+//  Registrations Resource Route
+Route::resource('registration', 'RegistrationController');
+
+//  Applications Resource Route
+Route::resource('applications', 'ApplicationsController');
+
+//  Courses Resource Route
+Route::resource('course', 'CoursesController');
 
 
 /* -------------------------------- */
@@ -58,14 +66,14 @@ Route::get('/login', function()
 
 });
 
-Route::get('/apply', function()
+Route::get('/signup', function()
 {
     //  If user is already authenticated, redirect to their main view - else login view.
     if (Auth::check()) {
         $user = User::find(Auth::id());
         return View::make('users.show')->with('user', $user);
     } else {
-        return View::make('users.apply');
+        return View::make('users.signup');
     }
 
 });
@@ -106,3 +114,16 @@ Route::get('/register', function()
 Route::post('/register', 'UsersController@store');
 
 
+//
+
+// Confide routes
+Route::get('users/create', 'UsersController@create');
+Route::post('users', 'UsersController@store');
+Route::get('users/login', 'UsersController@login');
+Route::post('users/login', 'UsersController@doLogin');
+Route::get('users/confirm/{code}', 'UsersController@confirm');
+Route::get('users/forgot_password', 'UsersController@forgotPassword');
+Route::post('users/forgot_password', 'UsersController@doForgotPassword');
+Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
+Route::post('users/reset_password', 'UsersController@doResetPassword');
+Route::get('users/logout', 'UsersController@logout');
