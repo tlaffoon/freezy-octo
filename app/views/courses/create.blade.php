5aa9332c7745d3bb255fdb83bbfd6bbf46a3f836
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2014-12-20 13:47:05 -0600

    Updated views for courses.

---
+++ app/views/courses/create.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.master')
+
+@section('topscript')
+@stop
+
+@section('content')
+
+<div class="container">
+    <div class="row clearfix">
+        <div class="col-md-4 col-md-offset-4">
+
+            <h2 class="page-header">Manage Your Courses:</h2>
+
+            
+
+
+
+        </div>
+    </div>
+</div>
+
+
+@stop
+
+@section('bottomscript')
+@stop
---
+++ app/views/courses/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('topscript')
+@stop
+
+@section('content')
+@stop
+
+@section('bottomscript')
+@stop
