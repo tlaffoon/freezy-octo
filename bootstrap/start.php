c35c66d0344d877e698924e8b06e0bff07bd5aa9
ryanorsinger <ryan.orsinger@gmail.com>
2015-01-06 12:52:51 -0600

    Finish ENV refactor in start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,18 +24,15 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-// $env = $app->detectEnvironment(array(
-// 	'local' => array('homestead'),
-// ));
-
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function()
+{
 
     if (!empty($_SERVER['LARAVEL_ENV'])) {
         return $_SERVER['LARAVEL_ENV'];
     }
 
     return 'local';
-));
+});
 
 /*
 |--------------------------------------------------------------------------
