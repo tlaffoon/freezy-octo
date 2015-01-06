af1faeaac3962a9525f8fe49de6568cfa285fe1a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-06 13:39:10 -0600

    Removed confide/entrust.

--- app/config/app.php
+++ app/config/app.php
@@ -121,8 +121,6 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-		'Zizaco\Confide\ServiceProvider',
-		// 'Zizaco\Entrust\EntrustServiceProvider'
 	),
 
 	/*
@@ -189,8 +187,6 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-		'Confide' => 'Zizaco\Confide\Facade',
-		// 'Entrust'    => 'Zizaco\Entrust\EntrustFacade'
 	),
 
 );
