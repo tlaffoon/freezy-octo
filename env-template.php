099c13952f302bf085565fb840589eb6f16f02e0
ryanorsinger <ryan.orsinger@gmail.com>
2015-01-06 13:27:53 -0600

    Move addresses onto user table. Replace hard-coded passwords with ENV variables. Refactor seeder classes into their own files.

--- env-template.php
+++ env-template.php
@@ -5,6 +5,14 @@ return array(
     'DB_NAME' => '',
     'DB_USER' => '',
     'DB_PASS' => '',
+
     'ADMIN_USER' => '',
     'ADMIN_PASS' => '',
+    'ADMIN_EMAIL' => '',
+    'ADMIN_GENDER' => '',
+    'ADMIN_FIRSTNAME' => '',
+    'ADMIN_LASTNAME' => '',
+    'ADMIN_FULLNAME' => '',
+    'ADMIN_PHONE' => '',
 );
+
