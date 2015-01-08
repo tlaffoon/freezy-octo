0b29570b4e8c83ec908e2319493a2c85839ed76a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-08 09:31:56 -0600

    Minor style change.

--- app/models/Application.php
+++ app/models/Application.php
@@ -10,16 +10,21 @@ class Application extends Eloquent {
         'course_id' => 'required|integer',
         'employment_status' => 'required',
         'financing_status' => 'required',
-        'bg_info' => 'required',
+        'bg_info' => 'required'
     ];
 
+
+    // Relationship allowing display of all a user's applications.
     public function user() {
         return $this->belongsTo('User');
     }
 
+
+    // Relationship allowing display of all applications to a particular course.
     public function course() {
         return $this->belongsTo('Course');
     }
+
 
     /**
      * The directory for resume uploads.
@@ -36,11 +41,11 @@ class Application extends Eloquent {
      */
     public function addUploadedResume($resume) {
         $systemPath = public_path() . '/' . $this->docDir . '/';
-        //$resumeName = $this->id . '_' . $resume->getClientOriginalName();
-        $resumeName = $resume->getClientOriginalName();
+        $resumeName = $this->id . '_' . $resume->getClientOriginalName();
         $resume->move($systemPath, $resumeName);
-        $this->resume_path = '/' . $this->docDir . '/' . $resumeName;
+        $this->resume_path = $this->docDir . '/' . $resumeName;
     }
+
 
     // Formats background information.
     public function setBGInfo($string) {
@@ -53,6 +58,7 @@ class Application extends Eloquent {
 
         $this->attributes['bg_info'] = $formattedInput;
     }
+
 
     // Formats questions submitted.
     public function setQuestions($string) {
