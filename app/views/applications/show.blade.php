15753ca167b2eb0999f6bd7a6eb06df8ac33843f
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-11 05:46:41 -0600

    Minor alteration on view.

--- app/views/applications/show.blade.php
+++ app/views/applications/show.blade.php
@@ -49,24 +49,24 @@
                     @endif
                     
                     <p> <strong> Submitted at: </strong> {{ $application->created_at }}</p>
-                    <p> <strong> Applying to: </strong>  {{ $application->course->name }}                              </p>
+                    <p> <strong> Applying to: </strong>  {{ $application->course->name }}</p>
                     
-                    <p> <strong> Phone: </strong> {{ $application->user->phone }}     </p>
-                    <p> <strong> Email: </strong> {{ $application->user->email }}     </p>
-                    <p> <strong> Address: </strong> {{ $application->user->address }}    </p>
+                    <p> <strong> Phone: </strong> {{ $application->user->phone }}</p>
+                    <p> <strong> Email: </strong> {{ $application->user->email }}</p>
+                    <p> <strong> Address: </strong> {{ $application->user->address }}</p>
 
-                    <p> <strong> Employment status: </strong> {{ ucfirst($application->employment_status) }}   </p>
+                    <p> <strong> Employment status: </strong> {{ ucfirst($application->employment_status) }}</p>
                     
                     @if ($application->resume_path)
-                        <p> <strong> Link to resume: </strong> {{ $application->resume_path }} </p>
+                        <p> <strong> Link to resume: </strong> {{ $application->resume_path }}</p>
                     @else 
-                        <p> <strong> Link to resume: </strong> No resume uploaded. </p>
+                        <p> <strong> Link to resume: </strong> No resume uploaded.</p>
                     @endif
 
-                    <p> <strong> Financing: </strong> {{ ucfirst($application->financing_status) }}            </p>
-                    <p> <strong> Referred by: </strong> {{ ucfirst($application->referred_by) }}               </p>
-                    <p> <strong> Background Info: </strong> {{ $application->bg_info }}                        </p>
-                    <p> <strong> Questions: </strong> {{ $application->questions }}                            </p>
+                    <p> <strong> Financing: </strong> {{ ucfirst($application->financing_status) }}</p>
+                    <p> <strong> Referred by: </strong> {{ ucfirst($application->referred_by) }}</p>
+                    <p> <strong> Background Info: </strong> {{ $application->bg_info }}</p>
+                    <p> <strong> Questions: </strong> {{ $application->questions }}</p>
                 
                 </div> <!-- End end application block -->
 
