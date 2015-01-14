9914ff60ed406c424087f927714d97177c45e2db
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-13 20:31:06 -0600

    Added sidebar partial with new icons.

---
+++ app/views/partials/sidebar.blade.php
@@ -0,0 +1,16 @@
+<!-- Dashboard Sidebar -->
+<div class="col-md-2 text-left">
+
+    <h3 class="page-header">Sidebar</h3>
+    <ul class="sidebar-links">
+        <li class=""><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>  Applications</li>
+        <li class=""><i class="fa fa-users"></i>  Attendance</li>
+        <li class=""><span class="glyphicon glyphicon-list" aria-hidden="true"></span>  Courses</li>
+        <li class=""><i class="fa fa-beer"></i>  Events</li>
+        <li class=""><i class="fa fa-wechat"></i>  Feedback</li>
+        <li class=""><i class="fa fa-money"></i>  Financial</li>
+        <li class=""><i class="fa fa-mortar-board"></i>  Lessons</li>
+        <li class=""><i class="fa fa-male"></i>  Speakers</li>
+        <li class=""><i class="fa fa-users"></i>  Users</li>
+    </ul>
+</div>
