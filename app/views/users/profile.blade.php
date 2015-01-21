0369287ba416beb97833b423636ccdc37288b5f0
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-21 00:38:26 -0600

    Yes.

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -111,7 +111,7 @@
                             @foreach ($user->applications as $application)
                             <!-- Begin Individual Application Block -->
                                 <div class="col-md-12 profile-application-header">
-                                    <h4> {{ $application->course->type }} - <span class="text-info">{{ ucfirst($application->status) }}</span>
+                                    <h4> {{ $application->course->designation }} | {{ $application->course->type }} - <span class="text-info">{{ ucfirst($application->status) }}</span>
 
                                         <div class="btn-group btn-group-user-profile pull-right">
 
