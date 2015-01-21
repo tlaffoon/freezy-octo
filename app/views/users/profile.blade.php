c2cd6fca5cd5a6e2b9a29d8f7b288d2773e00cdb
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-21 00:11:00 -0600

    View.

--- app/views/users/profile.blade.php
+++ app/views/users/profile.blade.php
@@ -111,7 +111,7 @@
                             @foreach ($user->applications as $application)
                             <!-- Begin Individual Application Block -->
                                 <div class="col-md-12 profile-application-header">
-                                    <h4> {{ $application->course->type }}
+                                    <h4> {{ $application->course->type | Status: $application->status}}
 
                                         <div class="btn-group btn-group-user-profile pull-right">
 
