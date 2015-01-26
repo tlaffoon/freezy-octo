b2f28c08e8b7e29797d9f03c2e4aa344cc7dd3b6
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-25 18:18:51 -0600

    Updated notification model.

--- app/models/Notification.php
+++ app/models/Notification.php
@@ -2,10 +2,9 @@
 
 class Notification extends \BaseModel
 {
-
-    protected $table = 'notifications';
-
-    protected $fillable   = ['user_id', 'subject', 'body', 'object_id', 'object_type', 'sent_at'];
+    protected $fillable   = ['user_id', 'type', 'subject', 'body', 'object_id', 'object_type', 'sent_at'];
+ 
+    private $relatedObject = null;
  
     public function getDates()
     {
@@ -16,28 +15,33 @@ class Notification extends \BaseModel
     {
         return $this->belongsTo('User');
     }
-
+ 
+    public function scopeUnread($query)
+    {
+        return $query->where('is_read', '=', 0);
+    }
+ 
     public function withSubject($subject)
     {
         $this->subject = $subject;
-     
+ 
         return $this;
     }
-     
+ 
     public function withBody($body)
     {
         $this->body = $body;
-     
+ 
         return $this;
     }
-     
+ 
     public function withType($type)
     {
-        $this->object_type = $type;
-     
+        $this->type = $type;
+ 
         return $this;
     }
-     
+ 
     public function regarding($object)
     {
         if(is_object($object))
@@ -45,20 +49,46 @@ class Notification extends \BaseModel
             $this->object_id   = $object->id;
             $this->object_type = get_class($object);
         }
-     
+ 
         return $this;
     }
-
+ 
     public function deliver()
     {
         $this->sent_at = new Carbon;
         $this->save();
-     
+ 
         return $this;
     }
-
-    public function scopeUnread($query)
+ 
+    public function hasValidObject()
     {
-        return $query->where('is_read', '=', 0);
+        try
+        {
+            $object = call_user_func_array($this->object_type . '::findOrFail', [$this->object_id]);
+        }
+        catch(\Exception $e)
+        {
+            return false;
+        }
+ 
+        $this->relatedObject = $object;
+ 
+        return true;
+    }
+ 
+    public function getObject()
+    {
+        if($this->relatedObject)
+        {
+            $hasObject = $this->hasValidObject();
+ 
+            if(!$hasObject)
+            {
+                throw new \Exception(sprintf("No valid object (%s with ID %s) associated with this notification.", $this->object_type, $this->object_id));
+            }
+        }
+ 
+        return $this->relatedObject;
     }
 }
