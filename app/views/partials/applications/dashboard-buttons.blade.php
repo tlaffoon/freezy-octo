e874a7185d74940c756629bb3976864ac766146f
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-30 18:08:47 -0600

    Made the buttons tiny.

--- app/views/partials/applications/dashboard-buttons.blade.php
+++ app/views/partials/applications/dashboard-buttons.blade.php
@@ -1,7 +1,7 @@
 <div class="btn-group btn-group-dashboard pull-right">
 
     <!-- Contact Info Modal Button Trigger -->
-    <a href="#infoModal" class="btn btn-default" role="button" rel="tooltip" data-original-title="Info" data-toggle="modal" data-target="#infoModal_{{ $application->id}}">
+    <a href="#infoModal" class="btn btn-default btn-sm" role="button" rel="tooltip" data-original-title="Info" data-toggle="modal" data-target="#infoModal_{{ $application->id}}">
         <i class="fa fa-info-circle"></i>
     </a>
 
@@ -12,21 +12,21 @@
 
 
     <!-- Application Inspect Button Trigger -->
-    <a href="{{ action('ApplicationsController@show', $application->id) }}" class="btn btn-default" role="button" rel="tooltip" data-original-title="Inspect Application">
+    <a href="{{ action('ApplicationsController@show', $application->id) }}" class="btn btn-default btn-sm" role="button" rel="tooltip" data-original-title="Inspect Application">
         <i class="fa fa-search"></i>
     </a>
 
     <!-- Send Message Modal Button Trigger -->
-    <a href="#messageModal" class="btn btn-default" role="button" rel="tooltip" data-original-title="Send Email" data-toggle="modal" data-target="#messageModal_{{ $application->id}}">
+    <a href="#messageModal" class="btn btn-default btn-sm" role="button" rel="tooltip" data-original-title="Send Email" data-toggle="modal" data-target="#messageModal_{{ $application->id}}">
         <i class="fa fa-envelope-o"></i>
     </a>
 
     <!-- Make Comment Modal Button Trigger -->
-    <a href="#commentModal" class="btn btn-default" role="button" rel="tooltip" data-original-title="Add Comment" data-toggle="modal" data-target="#commentModal_{{ $application->id}}">
+    <a href="#commentModal" class="btn btn-default btn-sm" role="button" rel="tooltip" data-original-title="Add Comment" data-toggle="modal" data-target="#commentModal_{{ $application->id}}">
         <i class="fa fa-comment-o"></i>
     </a>
 
-    <a id="{{$application->id}}" href="" class="btn btn-default btn-display" data-toggle="tooltip" data-placement="top" title="Toggle">
+    <a id="{{$application->id}}" href="" class="btn btn-default btn-display btn-sm" data-toggle="tooltip" data-placement="top" title="Toggle">
         <i class="fa fa-chevron-down"></i>
     </a>
     
