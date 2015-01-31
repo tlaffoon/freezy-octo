6319084b5d9d122dfa146adf7012651bca24b217
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-01-30 20:16:56 -0600

    Updated.

--- app/views/partials/applications/pending-list.blade.php
+++ app/views/partials/applications/pending-list.blade.php
@@ -54,10 +54,6 @@
                         <p> <strong> Background Info: </strong> {{ $application->bg_info }}                        </p>
                         <p> <strong> Questions: </strong> {{ $application->questions }}                            </p>
                     
-                        <hr>
-                        <p><strong> Comments: </strong></p>
-                        @include('partials.applications.comments-box')
-                    
                     </div>
                     <!-- End Individual Application Block -->
 
