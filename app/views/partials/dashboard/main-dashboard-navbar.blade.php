929ce8ab4874239bc86fbd8f9052aaa08494aa9a
Thomas J. Laffoon <tjl@thomasjlaffoon.com>
2015-02-01 23:58:16 -0600

    UPdated layout.

--- app/views/partials/dashboard/main-dashboard-navbar.blade.php
+++ app/views/partials/dashboard/main-dashboard-navbar.blade.php
@@ -11,10 +11,6 @@
     <!-- /.navbar-header -->
 
     <ul class="nav navbar-top-links navbar-right">
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
-            </a>
             
         <!-- /.dropdown -->
         <li class="dropdown">
